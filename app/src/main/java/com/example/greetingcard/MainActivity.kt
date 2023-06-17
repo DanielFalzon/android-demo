9e170c78916c6d78a64a2ba0a11b656e94d06749
DanielFalzon <danie.falzon6@gmail.com>
2023-06-17 13:21:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+    // onCreate function == main()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContent invokes @Composable functions. Used by Jetpack Compose to generate UI
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    GreetingText(message = "Happy Birthday General", from = "From Daniel")
+                }
+            }
+        }
+    }
+}
+
+//Composable function names start with capital letters and return Unit (no return type)
+//Column and row accept trailing lambda syntax as the last parameter is a function.
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+}
+
+//@Preview annotation allows you to build a preview of the app without building it.
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        GreetingText(message = "Happy Birthday General", from = "From Daniel")
+    }
+}
